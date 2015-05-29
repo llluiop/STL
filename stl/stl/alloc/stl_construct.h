acfb1e2ab8472791ee98d16081b6c90677e90c13
unknown <290522165@163.com>
2015-05-29 09:50:12 +0800

    files

--- stl/stl/alloc/stl_construct.h
+++ stl/stl/alloc/stl_construct.h
@@ -4,13 +4,13 @@
 #include <new>
 
 template<class T1, class T2>
-inline void _construct(T1* p, const T2& value)
+inline void construct(T1* p, const T2& value)
 {
 	new(p) T1(value);
 }
 
 template<class T>
-inline void _destory(T* p)
+inline void destory(T* p)
 {
 	p->~T();
 }
@@ -24,8 +24,25 @@ inline void destroy(ForwardIter first, ForwardIter last)
 template<class ForwardIter, class T>
 inline void __destroy(ForwardIter first, ForwardIter last, T*)
 {
+	typedef typename __type_traits<T>::has_trivail_destructor trivial_destructor;
+	__destory_aux(first, last, trivial_destructor());
+}
+
+template<class ForwardIter>
+inline void __destory_aux(ForwardIter first, ForwardIter last, __false_type)
+{
+	for (; first < last; first++)
+		destroy(&*first);
+}
+
+template<class ForwardIter>
+inline void __destory_aux(ForwardIter first, ForwardIter last, __true_type)
+{
 
 }
+
+inline void destory(char*, char*){}
+inline void destory(wchar_t*, wchar_t*){}
 
 template<class T>
 class Alloc
