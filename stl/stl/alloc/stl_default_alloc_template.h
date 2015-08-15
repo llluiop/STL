8e26aa1428f160a2d637e159322679925abafedd
unknown <290522165@163.com>
2015-08-15 09:45:55 +0800

    add refill

--- stl/stl/alloc/stl_default_alloc_template.h
+++ stl/stl/alloc/stl_default_alloc_template.h
@@ -73,12 +73,72 @@ void* __default_alloc_template<thread, inst>::allocate(size_t n)
 	result = *my_free_list;
 	if (0 == result)
 	{
-		return refill(n);
+		return refill(ROUND_UP(n));
 	}
 
 	*my_free_list = result->free_list_link;
 
 	return result;
+}
+
+template<bool thread, int inst>
+void __default_alloc_template<thread, inst>::deallocate(void* p, size_t n)
+{
+	obj* q = (obj*)p;
+	obj* volatile *my_free_list = nullptr;
+
+
+	if (n > __max)
+	{
+		malloc_alloc:deallocate(p, n);
+		return;
+	}
+
+	my_free_list = free_list + FREELIST_INDEX(n);
+	q->free_list_link = *my_free_list;
+	*my_free_list = q;
+
+	return;
+
+}
+
+template<bool thread, int inst>
+void* __default_alloc_template<thread, inst>::refill(size_t n)
+{
+	int nobjs = 20;
+
+	char* chunk = chunk_alloc(n, nobjs);
+	if (1 == nobjs)
+	{
+		return chunk;
+	}
+
+	obj* volatile *my_free_list = nullptr;
+	obj* result = nullptr;
+	obj* current = nullptr;
+	obj* next = nullptr;
+
+	my_free_list = free_list + FREELIST_INDEX(n);
+
+	result = (obj*)chunk;
+	*my_free_list = next = (obj*)(chunk + n);
+	for (int i = 1; ; i++)
+	{
+		current = next;
+		next = (obj*)((char*)next + n);
+		if (nobjs - i == 1)
+		{
+			current->free_list_link = nullptr;
+			break;
+		}
+		else
+		{
+			current->free_list_link = next;
+		}
+	}
+	
+	return result;
+
 }
 
 #endif
